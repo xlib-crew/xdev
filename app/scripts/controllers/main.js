07bc303d137a0893d614156b9e316b8b0858962d
ph3b <mathiasiden@me.com>
2014-05-14 01:25:38 +0200

    mer greier

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('xdevApp')
-  .controller('MainCtrl', function ($scope, $localStorage) {
-    $scope.users = function(_username, _password){
-        $localStorage.$default({users: {username: _username, password: _password}})
-    };
-    $scope.users('Erlend', '4536234')
-    console.log($localStorage)
+  .controller('MainCtrl', function ($scope, $localStorage, templogin, $location) {
+    $scope.login = function(_username, _password){
+        if(templogin.login(_username,_password)){
+            console.log('hei')
+           $location.path('/home');
+        }
+        else {
+            console.log('Feil brukernavn/pass')
+        }
+    }
   });
--- app/scripts/services/templogin.js
+++ app/scripts/services/templogin.js
@@ -6,17 +6,16 @@ angular.module('xdevApp')
     // ...
 
     var userTable = {
-        1 : {username: 'Matt', password: '1234'},
+        1 : {username: 'Mathias', password: 'hawaii'},
         2 : {username: 'Erlend', password: '4321'}
     }
-
-    // Public API here
     return {
       login: function (_username, _password) {
           var key;
           for(key in userTable){
-              if(userTable[key].username == _username && userTable[key].password == _password);
-              return true;
+              if(userTable[key].username == _username && userTable[key].password == _password){
+                  return true;
+              }
           }
           return false;
       }
